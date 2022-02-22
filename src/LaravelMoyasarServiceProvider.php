3cb9c4db46d4d0ae15c5f9c655c0d487bba1a988
Roduan Kareem Aldeen <roduan98@gmail.com>
2022-02-22 14:30:23 +0200

    fix: migrations can't be exported

--- src/LaravelMoyasarServiceProvider.php
+++ src/LaravelMoyasarServiceProvider.php
@@ -21,8 +21,8 @@ class LaravelMoyasarServiceProvider extends PackageServiceProvider
             ->hasViews()
             ->hasViewComponents('moyasar', Init::class)
             ->hasRoute('web')
-            ->hasConfigFile('moyasar');
-        // ->hasMigration('create_laravel-moyasar_table')
+            ->hasConfigFile('moyasar')
+            ->hasMigration('create_transactions_table');
             // ->hasCommand(LaravelMoyasarCommand::class);
     }
 }
